e9334e699724e13652a2de1f63841064fa43e6ea
sarforajs <128588277+sarforajs@users.noreply.github.com>
2025-06-06 21:55:37 +0530

    Update UserService.java

---
+++ src/main/java/com/sarforaj/userservice/service/UserService.java
@@ -0,0 +1,24 @@
+package com.sarforaj.userservice.service;
+
+import com.sarforaj.userservice.model.User;
+import com.sarforaj.userservice.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+}
